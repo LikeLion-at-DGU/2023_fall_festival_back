ee53f36e47e894a5936df67fe2691052301a0354
Sangjun Lee <lsjmc0224@dgu.edu>
2023-09-26 14:54:29 +0900

    :sparkles: Add chat model
    chat 앱에 Chat (방명록) 모델 추가

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    key=models.CharField(
+        max_length=10,
+        blank=True,
+        editable=False
+    )
+    
+    def __str__(self):
+        return f'{self.booth}/{self.key}'
