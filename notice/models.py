8fe10cc6058ab6249294f4e55dd6ea0c8b8b49a6
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-09-27 11:09:48 +0900

    :adhesive_bandage: Fix booth, chat, notice 앱  models.py 수정
    booth의 description charfield maxlength 255 추가
    chat의 content charfield maxlength 100 추가
    notice의 Notification, Promotion 모델의 content필트 textfield로 수정

--- notice/models.py
+++ notice/models.py
@@ -8,7 +8,7 @@ class Notification(models.Model):
     # max_length 협의 후 수정
     title = models.CharField(max_length=255)
     # text, char 협의 후 수정 
-    content = models.CharField()
+    content = models.TextField()
     # 세부사항 수정 가능
     created_at = models.DateTimeField(null=True, blank=True)
     TYPE_CHOICES = (
@@ -26,7 +26,7 @@ class Promotion(models.Model):
     # max_length 협의 후 수정
     title = models.CharField(max_length=255)
     # text필드인지, char필드인지 협의 후 수정 
-    content = models.CharField()
+    content = models.TextField()
     created_at = models.DateTimeField(null=True, blank=True)
     TYPE_CHOICES = (
         # 카테고리
