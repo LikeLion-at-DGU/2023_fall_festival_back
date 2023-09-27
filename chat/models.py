730043dff54b77880b58499311bcd597e4c5b1d6
JongbeomLee623 <jambon23@dgu.ac.kr>
2023-09-28 02:18:43 +0900

    :adhesive_bandage: Fix chat model content field TextField로 수정
    chat model content field TextField로 수정 후 마이그레이션까지 진행했습니다.

--- chat/models.py
+++ chat/models.py
@@ -6,7 +6,7 @@ class Chat(models.Model):
     id = models.BigIntegerField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
     # min_length, max_length 10~100인데 프론트와 협의 후 변경
-    content = models.CharField(max_length=100)
+    content = models.TextField()
     key=models.CharField(
         max_length=10,
         blank=True,
